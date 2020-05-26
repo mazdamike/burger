fa92a727690dbe92e47c16563b7d080063b0edd2
mazdamike <mwpowell44@gmail.com>
2020-05-26 10:34:39 -0400

    finished server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
-var app = express();
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 var PORT = process.env.PORT || 8080;
+
+var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -15,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Handlebars
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
